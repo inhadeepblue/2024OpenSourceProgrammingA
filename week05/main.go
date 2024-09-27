5778224039414f0e2b89fedda2c9145b3a0909d0
inhadeepblue <deepblue@inha.ac.kr>
2024-09-27 12:24:19 +0900

    v0.3 Type

--- week05/main.go
+++ week05/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
+	//var i int = 55
+
+	// var i int
+	// i = 55
+
+	var f float32 = 4.30
+	//f := 4.30 // float64
+	i := 55
+
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Printf("%s\n", strings.Title("kim inha"))
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
 	fmt.Println("value i :", i)
