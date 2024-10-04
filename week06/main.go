8b4f60e836ad14e306c33fbf1f0d723d7c3e3d6f
inhadeepblue <deepblue@inha.ac.kr>
2024-10-04 11:24:35 +0900

    v0.7 Zero value

--- week06/main.go
+++ week06/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9
-	c1 := 'Z'
-	c2 := '김'
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero value
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f > float64(i), "\n") // comparsion (true/false)
 }
