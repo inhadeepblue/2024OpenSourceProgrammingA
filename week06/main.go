d2ddb1d0dd93d4c59ab85583ddf66b42f841bfc4
inhadeepblue <deepblue@inha.ac.kr>
2024-10-04 10:39:24 +0900

    v0.5 Conversion

--- week06/main.go
+++ week06/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	i := 13
-	f := 12.9
+	var f float64 = 12.9
 
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
 	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
