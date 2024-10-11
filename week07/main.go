81ffd47a90903c9db98fe2469e49b4bceebc3892
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 10:45:56 +0900

    v1.1 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	//var month int = int(now.Month())
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금은 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
-	fmt.Println(now.Month())
+	var army string = "우리는 ?가와 ?민에 충성을 다하는 대한민? 육군이다"
+	armyFixed := strings.NewReplacer("?", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
