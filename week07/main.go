47308a2bf01441ba48ae5c84aebdf9398c916527
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 12:00:29 +0900

    v1.4 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,16 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
-
+	fmt.Print("점수 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)                  // python strip
+	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 16진수로 처리
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
 	}
 }
