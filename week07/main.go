74ec142ebdc1cd64422cafe8675bac2ae91331ea
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 10:58:13 +0900

    v1.2 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	var army string = "우리는 ?가와 ?민에 충성을 다하는 대한민? 육군이다"
-	armyFixed := strings.NewReplacer("?", "국")
-	fmt.Println(army)
-	fmt.Println(armyFixed.Replace(army))
+	in := bufio.NewReader(os.Stdin)
+	fmt.Print("Input your name : ")
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
 }
