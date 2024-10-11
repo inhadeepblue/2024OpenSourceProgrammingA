c8b36f6cb57ee809484301967116fee4d30a91c3
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 12:06:17 +0900

    v1.5 Scope

--- week07/main.go
+++ week07/main.go
@@ -18,12 +18,12 @@ func main() {
 	}
 
 	i = strings.TrimSpace(i)                  // python strip
-	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 16진수로 처리
-	if score >= 60 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", score)
+	score, err := strconv.ParseInt(i, 10, 32) // 문자열 변수 i의 값을 정수형(32비트)으로 변환, 입력받은 값은 10진수로 처리
+	var aOrNot string
+	if score >= 90 {
+		aOrNot = "A"
 	} else {
-		fmt.Println("BCDF")
-		fmt.Printf("%d\n", score)
+		aOrNot = "BCDF"
 	}
+	fmt.Printf("%d점은 %s등급 입니다\n", score, aOrNot)
 }
