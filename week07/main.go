b3199506cfe390a325e6478e281a2d31dc2aa30b
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 10:28:42 +0900

    v1.0 Time

--- week07/main.go
+++ week07/main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	var now time.Time = time.Now()
-	var month int = int(now.Month())
-	fmt.Println(month)
+	//var month int = int(now.Month())
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
+	fmt.Printf("지금은 시각은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+	fmt.Println(now.Month())
 }
