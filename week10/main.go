3da88baa7edf0daeb24db9b3d355316dd979878b
inhadeepblue <deepblue@inha.ac.kr>
2024-11-01 11:32:36 +0900

    v2.7 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -25,7 +26,7 @@ func main() {
 	if n < 2 {
 		isPrime = false
 	} else {
-		for j := 2; j < n; j++ {
+		for j := 2; j <= int(math.Sqrt(float64(n))); j++ {
 			if n%j == 0 {
 				isPrime = false
 				break // 무의미한 반복을 제거
