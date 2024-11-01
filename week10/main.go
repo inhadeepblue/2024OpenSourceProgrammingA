573e4acb7f3043886528524c643750dbb6e330b0
inhadeepblue <deepblue@inha.ac.kr>
2024-11-01 10:51:40 +0900

    v2.4 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -20,14 +20,17 @@ func main() {
 	i = strings.TrimSpace(i)
 	n, err := strconv.Atoi(i)
 
-	counts := 0
+	// counts := 0
+	var isPrime bool = true  // 가독성, 저장공간크기 개선
 	for j := 2; j < n; j++ { // 2부터 입력된 수 앞까지 반복
 		if n%j == 0 { // 약수면
-			counts++ // 나누어 떨어지는 횟수 카운트
+			//counts++ // counts = counts + 1 , 더하기 연산 제거
+			isPrime = false
 		}
 	}
 
-	if counts == 0 {
+	//if counts == 0 {  // 나누어 떨어지는 수가 있으면 안됨
+	if isPrime { // == 비교 연산 제거
 		fmt.Printf("%d는(은) 소수입니다", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다", n)
