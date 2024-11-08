de129b370cfd9c385f43622c8d3ced1ec538e327
inhadeepblue <deepblue@inha.ac.kr>
2024-11-08 11:48:29 +0900

    v3.5 Prime number

--- week11/main.go
+++ week11/main.go
@@ -2,12 +2,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
+	"week11/keyboard"
 )
 
 func isPrime(n int) bool {
@@ -28,31 +25,15 @@ func isPrime(n int) bool {
 	return true
 }
 
-func getInteger() (int, error) {
-	r := bufio.NewReader(os.Stdin)
-	a, err := r.ReadString('\n')
-	if err != nil {
-		//log.Fatal(err)
-		return 0, err
-	}
-
-	a = strings.TrimSpace(a)
-	n, err := strconv.Atoi(a)
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
-}
-
 func main() {
 	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
-	n1, err := getInteger()
+	n1, err := keyboard.GetInteger()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Print("두 번째 정수(끝 값) 입력 : ")
-	n2, err := getInteger()
+	n2, err := keyboard.GetInteger()
 	if err != nil {
 		log.Fatal(err)
 	}
