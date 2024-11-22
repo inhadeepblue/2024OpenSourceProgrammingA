4177519b3944efc3b85069479a3c4ce59935186f
inhadeepblue <deepblue@inha.ac.kr>
2024-11-22 11:27:26 +0900

    v4.3 Slice

---
+++ week13/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
+	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
+	//gpa_slice[1] = 2.71
+	gpa[2] = 2.71
+	gpa_slice = append(gpa_slice, 4.3)
+	fmt.Println(len(gpa_slice), gpa_slice, gpa)
+}
