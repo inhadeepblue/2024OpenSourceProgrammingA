a170793279a280e39f10de9322776095f9c9ac1c
inhadeepblue <deepblue@inha.ac.kr>
2024-11-22 11:29:45 +0900

    v4.4 Slice

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	var emptySlice []int
+	//emptySlice = make([]int, 5)
+	fmt.Printf("%#v\n", emptySlice)
+
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
 	//gpa_slice[1] = 2.71
